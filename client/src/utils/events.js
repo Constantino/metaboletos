78c31f230aa2bf5c68d23a1a5bbea9b47766323c
Edgar Sanchz <edgar.sanchez@3pillarglobal.com>
2022-08-20 14:37:51 -0500

    Add more info to events data

---
+++ client/src/utils/events.js
@@ -0,0 +1,91 @@
+import event1 from "../media/banksy.png";
+import event2 from "../media/banksy.png";
+import event3 from "../media/banksy.png";
+import event4 from "../media/banksy.png";
+import event5 from "../media/banksy.png";
+import event6 from "../media/banksy.png";
+import event7 from "../media/banksy.png";
+import event8 from "../media/banksy.png";
+
+export const event_nfts = [
+  {
+    id: 1,
+    img: event1,
+    description: "This is the description event with lots of marketing stuff.",
+    name: "Event 1",
+    date: "2 Sep",
+    duration: "10:00 PM - 01:00 AM",
+    ownerName: "Garo",
+    ownerAvatar: "",
+  },
+  {
+    id: 2,
+    img: event2,
+    description: "This is the description event with lots of marketing stuff.",
+    name: "Event 1",
+    date: "2 Sep",
+    duration: "10:00 PM - 01:00 AM",
+    ownerName: "Garo",
+    ownerAvatar: "",
+  },
+  {
+    id: 3,
+    img: event3,
+    description: "This is the description event with lots of marketing stuff.",
+    name: "Event 1",
+    date: "2 Sep",
+    duration: "10:00 PM - 01:00 AM",
+    ownerName: "Garo",
+    ownerAvatar: "",
+  },
+  {
+    id: 4,
+    img: event4,
+    description: "This is the description event with lots of marketing stuff.",
+    name: "Event 1",
+    date: "2 Sep",
+    duration: "10:00 PM - 01:00 AM",
+    ownerName: "Garo",
+    ownerAvatar: "",
+  },
+  {
+    id: 5,
+    img: event5,
+    description: "This is the description event with lots of marketing stuff.",
+    name: "Event 1",
+    date: "2 Sep",
+    duration: "10:00 PM - 01:00 AM",
+    ownerName: "Garo",
+    ownerAvatar: "",
+  },
+  {
+    id: 6,
+    img: event6,
+    description: "This is the description event with lots of marketing stuff.",
+    name: "Event 1",
+    date: "2 Sep",
+    duration: "10:00 PM - 01:00 AM",
+    ownerName: "Garo",
+    ownerAvatar: "",
+  },
+  {
+    id: 7,
+    img: event7,
+    description: "This is the description event with lots of marketing stuff.",
+    name: "Event 1",
+    date: "2 Sep",
+    duration: "10:00 PM - 01:00 AM",
+    ownerName: "Garo",
+    ownerAvatar: "",
+  },
+  {
+    id: 8,
+    img: event8,
+    description: "This is the description event with lots of marketing stuff.",
+    name: "Event 1",
+    date: "2 Sep",
+    duration: "10:00 PM - 01:00 AM",
+    ownerName: "Garo",
+    ownerAvatar: "",
+  },
+];
