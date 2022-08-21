ea4a85a4a70be7c7bcb9aa4426ac12bbd8c0c9a2
Garo San <garosanchez27@gmail.com>
2022-08-20 20:22:54 -0500

    Dynamically store NFT based on id passed

--- service/index.mjs
+++ service/index.mjs
@@ -1,7 +1,7 @@
-import express from 'express';
+import express from "express";
 import cors from "cors";
-import 'dotenv/config';
-import storeNFT from './services/nft.storage.service.mjs';
+import "dotenv/config";
+import storeNFT from "./services/nft.storage.service.mjs";
 
 const app = express();
 const port = process.env.PORT;
@@ -9,17 +9,20 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('hello');
+app.get("/", (req, res) => {
+  res.send("hello");
 });
 
-app.get('/upload/nft', async (req, res) => {    
-    const result = await storeNFT();
-    res.status(200).send(result);
+app.post("/upload/nft", async (req, res) => {
+  console.log("req", req.body);
+  const result = await storeNFT(req.body.id);
+  res.status(200).send(result);
 });
 
-app.get('/read/nft', (req, res) => {
-    res.send('read nft');
+app.get("/read/nft", (req, res) => {
+  res.send("read nft");
 });
 
- app.listen(port, () => console.log('App is listening on url http://localhost:' + port));
+app.listen(port, () =>
+  console.log("App is listening on url http://localhost:" + port)
+);
